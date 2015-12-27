df91157af4a4850a5017128ed6248697832fa662
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2015-12-27 10:58:09 +0900

    fix: tests add case-1

--- test/1/cleanup.sh
+++
@@ -1,9 +0,0 @@
-files="
-/etc/default/swarm-manager
-/etc/systemd/system/swarm-manager.service
-"
-
-for file in $files;
-do
-    [ -f $file ] && rm -rf $file
-done
--- test/1/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-[ -f /etc/default/swarm-manager ] || exit 1
-[ -f /etc/systemd/system/swarm-manager.service ] || exit 1
-
-diff 1/swarm-manager /etc/default/swarm-manager || exit 1
-diff 1/swarm-manager.service /etc/default/swarm-manager.service || exit 1
